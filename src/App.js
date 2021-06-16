85d31f03a681808f631ccb30922f2fd7e165fef5
alojzy231 <alojzy231@gmail.com>
2021-06-16 17:34:28 +0200

    Finished about section
    -google map
    -brief history about restaurant

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 import Background from './Background'
 import Navigation from './Navbar';
 import Home from './sections/Home';
+import About from './sections/About';
 import Menu from './sections/Menu/Menu';
 import Modal from './sections/Menu/Modal';
 
@@ -23,6 +24,7 @@ function App(props) {
         <Switch>
             <Route path="/home" component={Home} />
             <Route path="/menu" component={Menu} />
+            <Route path="/about" component={About} />
             
             <Route path="/">
               <Redirect to="/home" />
